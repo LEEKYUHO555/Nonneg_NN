b0bd3e21429cf502d7392b4cdc1364599858a07a
LEEKYUHO555 <kueho0716@gmail.com>
2019-12-13 16:09:56 +0900

    refresh_tensorboard_log

--- examples/mnist_mlp_tensorflow.py
+++ examples/mnist_mlp_tensorflow.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
+import os, re, os.path
+mypath = "C:\\Users\\LEEKYUHO\\Desktop\\Code\\keras\\examples\\board\mnist"
+for root, dirs, files in os.walk(mypath):
+    for file in files:
+        os.remove(os.path.join(root, file))
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
 shifting_value_W1 = 0.5
+shifting_value_W2 = 1
 
 #########
 # 신경망 모델 구성
@@ -32,13 +38,21 @@ w2_hist = tf.summary.histogram("weight2", W2)
 with tf.name_scope("cost") as scope:
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 
-with tf.name_scope("power") as scope:
+with tf.name_scope("power_W1") as scope:
     ref_W1 = tf.abs(W1)
-    shifting_value_tensor = tf.constant(shifting_value_W1, shape=[784,150])
-    nonneg_W1 = tf.abs(tf.add(W1,shifting_value_tensor))
-    ref_sum = tf.reduce_sum(tf.matmul(X, ref_W1))
-    nonneg_sum = tf.add(tf.reduce_sum(tf.matmul(X, nonneg_W1)), tf.reduce_sum(tf.scalar_mul(0.5, X)))
-    power_ratio = tf.divide(nonneg_sum, ref_sum)
+    shifting_value_tensor_W1 = tf.constant(shifting_value_W1, shape=[784,150])
+    nonneg_W1 = tf.abs(tf.add(W1, shifting_value_tensor_W1))
+    ref_sum_W1 = tf.reduce_sum(tf.matmul(X, ref_W1))
+    nonneg_sum_W1 = tf.add(tf.reduce_sum(tf.matmul(X, nonneg_W1)), tf.reduce_sum(tf.scalar_mul(0.5, X)))
+    power_ratio_W1 = tf.divide(nonneg_sum_W1, ref_sum_W1)
+
+# with tf.name_scope("power_W2") as scope:
+#     ref_W2 = tf.abs(W2)
+#     shifting_value_tensor_W2 = tf.constant(shifting_value_W2, shape=[150,10])
+#     nonneg_W2 = tf.abs(tf.add(W1, shifting_value_tensor_W2))
+#     ref_sum_W2 = tf.reduce_sum(tf.matmul(X, ref_W2))
+#     nonneg_sum_W2 = tf.add(tf.reduce_sum(tf.matmul(X, nonneg_W2)), tf.reduce_sum(tf.scalar_mul(0.5, X)))
+#     power_ratio_W2 = tf.divide(nonneg_sum_W2, ref_sum_W2)
 
 with tf.name_scope("train") as scope:
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
@@ -81,7 +95,7 @@ print('최적화 완료!')
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
-accuracy_val, P_ratio = sess.run([accuracy, power_ratio],
+accuracy_val, P_ratio = sess.run([accuracy, power_ratio_W1],
                         feed_dict={X: mnist.test.images,
                                    Y: mnist.test.labels})
 
