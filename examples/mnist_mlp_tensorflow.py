7c8223f14c3b10af44d0ea371954cc1705987acf
LEEKYUHO555 <kueho0716@gmail.com>
2019-12-26 17:35:24 +0900

    direct_demonstration_of_shifted_NN

--- examples/mnist_mlp_tensorflow.py
+++ examples/mnist_mlp_tensorflow.py
@@ -8,12 +8,14 @@ for root, dirs, files in os.walk(mypath):
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
+EPOCH = 10
 shifting_value_W1 = 0.5
 shifting_value_W2 = 1.0
 
 #####################
 ## 신경망 모델 구성 ##
 #####################
+
 # with tf.name_scope("input") as scope:
 #     X = tf.placeholder(tf.float32, [None, 784])
 #
@@ -32,7 +34,7 @@ shifting_value_W2 = 1.0
 # with tf.name_scope("layer2") as scope:
 #     model = tf.matmul(L1, W2)
 
-with tf.variable_scope("MyScope"):
+with tf.variable_scope("Non_shifted"):
     X = tf.placeholder(tf.float32, [None, 784])
     Y = tf.placeholder(tf.float32, [None, 10])
     W1 = tf.get_variable("W1", shape=[784, 150], initializer=tf.contrib.layers.xavier_initializer(),
@@ -42,11 +44,23 @@ with tf.variable_scope("MyScope"):
                          constraint=lambda x: tf.clip_by_value(x, -shifting_value_W2, shifting_value_W2))
     model = tf.matmul(L1, W2)
 
-w1_hist = tf.summary.histogram("weight1", W1)
-w2_hist = tf.summary.histogram("weight2", W2)
+with tf.variable_scope("Shifted"):
+    shifting_value_tensor_W1 = tf.constant(shifting_value_W1, shape=[784, 150])
+    shifted_W1 = tf.add(W1, shifting_value_tensor_W1)
+    shifting_value_tensor_W2 = tf.constant(shifting_value_W2, shape=[150, 10])
+    shifted_W2 = tf.add(W2, shifting_value_tensor_W2)
 
-with tf.name_scope("cost") as scope:
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
+    sum_X = tf.reduce_sum(X, 1, keepdims=True)
+    Compensate_X = tf.ones([1,150])*sum_X
+    # Compensate_X = tf.transpose(tf.tile(sum_X, [150, 1]))
+    shifted_H1 = tf.matmul(X, shifted_W1) - tf.scalar_mul(shifting_value_W1, Compensate_X)
+    shifted_L1 = tf.nn.relu(shifted_H1)
+
+    sum_L1 = tf.reduce_sum(shifted_L1, 1, keepdims=True)
+    Compensate_L1 = tf.ones([1,10])*sum_L1
+    # Compensate_L1 = tf.transpose(tf.tile(sum_L1, [10, 1]))
+    shifted_H2 = tf.matmul(shifted_L1, shifted_W2) - tf.scalar_mul(shifting_value_W2, Compensate_L1)
+    shifted_model = shifted_H2
 
 with tf.name_scope("power_W1") as scope:
     ref_W1 = tf.abs(W1)
@@ -64,6 +78,12 @@ with tf.name_scope("power_W2") as scope:
     nonneg_sum_W2 = tf.add(tf.reduce_sum(tf.matmul(L1, nonneg_W2)), tf.reduce_sum(tf.scalar_mul(shifting_value_W2, L1)))
     power_ratio_W2 = tf.divide(nonneg_sum_W2, ref_sum_W2)
 
+with tf.name_scope("tensor_board") as scope:
+    w1_hist = tf.summary.histogram("weight1", W1)
+    w2_hist = tf.summary.histogram("weight2", W2)
+
+with tf.name_scope("cost") as scope:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 
 with tf.name_scope("train") as scope:
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
@@ -82,7 +102,7 @@ sess.run(init)
 batch_size = 100
 total_batch = int(mnist.train.num_examples / batch_size)
 
-for epoch in range(50):
+for epoch in range(EPOCH):
     total_cost = 0
 
     for i in range(total_batch):
@@ -106,8 +126,10 @@ print('최적화 완료!')
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
-accuracy_val, P_ratio_W1, P_ratio_W2 = sess.run([accuracy, power_ratio_W1, power_ratio_W2],
-                        feed_dict={X: mnist.test.images,
-                                   Y: mnist.test.labels})
+is_correct_shifted = tf.equal(tf.argmax(shifted_model, 1), tf.argmax(Y, 1))
+shifted_accuracy = tf.reduce_mean(tf.cast(is_correct_shifted, tf.float32))
 
-print('정확도:', accuracy_val, 'P_ratio_W1 : ', P_ratio_W1, 'P_ratio_W2 : ', P_ratio_W2)
+accuracy_val, shifted_accuracy_val, P_ratio_W1, P_ratio_W2 = sess.run(
+    [accuracy, shifted_accuracy, power_ratio_W1, power_ratio_W2], feed_dict={X: mnist.test.images, Y: mnist.test.labels})
+
+print('Acc:', accuracy_val, 'Shifted_Acc:', shifted_accuracy_val, 'P_ratio_W1 : ', P_ratio_W1, 'P_ratio_W2 : ', P_ratio_W2)
