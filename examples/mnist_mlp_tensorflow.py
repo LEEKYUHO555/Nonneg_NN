47317850565676e9474dfcaa0bfdfb8980b985ba
LEEKYUHO555 <kueho0716@gmail.com>
2019-12-26 15:35:25 +0900

    clipping_done

--- examples/mnist_mlp_tensorflow.py
+++ examples/mnist_mlp_tensorflow.py
@@ -11,25 +11,35 @@ mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 shifting_value_W1 = 0.5
 shifting_value_W2 = 1.0
 
-#########
-# 신경망 모델 구성
-######
-with tf.name_scope("input") as scope:
+#####################
+## 신경망 모델 구성 ##
+#####################
+# with tf.name_scope("input") as scope:
+#     X = tf.placeholder(tf.float32, [None, 784])
+#
+# with tf.name_scope("y_") as scope:
+#     Y = tf.placeholder(tf.float32, [None, 10])
+#
+# with tf.name_scope("weight1") as scope:
+#     W1 = tf.Variable(tf.random_normal([784, 150], stddev=0.01))
+#
+# with tf.name_scope("layer1") as scope:
+#     L1 = tf.nn.relu(tf.matmul(X, W1))
+#
+# with tf.name_scope("weight2") as scope:
+#     W2 = tf.Variable(tf.random_normal([150, 10], stddev=0.01))
+#
+# with tf.name_scope("layer2") as scope:
+#     model = tf.matmul(L1, W2)
+
+with tf.variable_scope("MyScope"):
     X = tf.placeholder(tf.float32, [None, 784])
-
-with tf.name_scope("y_") as scope:
     Y = tf.placeholder(tf.float32, [None, 10])
-
-with tf.name_scope("weight1") as scope:
-    W1 = tf.Variable(tf.random_normal([784, 150], stddev=0.01))
-
-with tf.name_scope("layer1") as scope:
+    W1 = tf.get_variable("W1", shape=[784, 150], initializer=tf.contrib.layers.xavier_initializer(),
+                         constraint=lambda x: tf.clip_by_value(x, -shifting_value_W1, shifting_value_W1))
     L1 = tf.nn.relu(tf.matmul(X, W1))
-
-with tf.name_scope("weight2") as scope:
-    W2 = tf.Variable(tf.random_normal([150, 10], stddev=0.01))
-
-with tf.name_scope("layer2") as scope:
+    W2 = tf.get_variable("W2", shape=[150, 10], initializer=tf.contrib.layers.xavier_initializer(),
+                         constraint=lambda x: tf.clip_by_value(x, -shifting_value_W2, shifting_value_W2))
     model = tf.matmul(L1, W2)
 
 w1_hist = tf.summary.histogram("weight1", W1)
@@ -55,8 +65,6 @@ with tf.name_scope("power_W2") as scope:
     power_ratio_W2 = tf.divide(nonneg_sum_W2, ref_sum_W2)
 
 
-
-
 with tf.name_scope("train") as scope:
     optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
@@ -66,7 +74,7 @@ with tf.Session() as sess:
 
 #########
 # 신경망 모델 학습
-######
+#########
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
@@ -94,7 +102,7 @@ print('최적화 완료!')
 
 #########
 # 결과 확인
-######
+#########
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
